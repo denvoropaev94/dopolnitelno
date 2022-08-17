7cc7f652bbe6b52fec3e998c351572b83495caff
«Denis» <denvoropaev7@gmail.com>
2022-08-17 12:43:56 +0300

    Задача,которая принимает на вход число и выдаёт сумму цифр в числе

---
+++ sum_numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+Console.WriteLine("Введите число: ");
+string? number = Console.ReadLine();
+
+bool isNum = int.TryParse(number, out int result);
+if (isNum == false)
+{
+    Console.WriteLine("Введите число, а не символы какие-то!!! ");
+    return;
+}
+int ReturnsSumNumbers(string num)
+{
+    int summ = 0;
+    for (int i = 0; i < num.Length; i++)
+    {
+        if (char.IsDigit(num[i]))
+        {
+            summ += Convert.ToInt32(num[i].ToString());
+        }
+    }
+return summ;
+}
+
+int res = ReturnsSumNumbers(number);
+Console.WriteLine(res);
